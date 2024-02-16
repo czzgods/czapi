4865cbd0f6e696dae90e2b9652eed706fa12212e
cz <3302076969@qq.com>
2024-02-16 20:40:40 +0800

    使用后端项目模版进行项目初始化

---
+++ src/main/java/com/cz/czapi/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.cz.czapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cz.czapi.model.entity.User;
+
+/**
+ * @Entity com.yupi.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
